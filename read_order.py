f9688fd439b6049562e0e4e0c69246800767b65b
laxmikanth-d <laxmikanth.dhage@gmail.com>
2020-10-25 00:26:24 -0700

    improvements

--- read_order.py
+++ read_order.py
@@ -5,11 +5,10 @@ from config import Config
 import os
 import boto3
 
-
 conn = None
 params = Config()
 conn = ps.connect(**params)
-
+WORKING_BUCKET = 'download-testing-10202020'
 
 def read_order():
 
@@ -25,36 +24,38 @@ def read_order():
             aws_access_key_id=aws_access_key_id,
             aws_secret_access_key=aws_secret_access_key
         )
-
         
-        files = s3.list_objects_v2(Bucket='download-testing-10202020', Prefix='sample')
+        files = s3.list_objects_v2(Bucket=WORKING_BUCKET, Prefix='sample')
 
-        for file in files['Contents']:
-            # print(file['Key'])
+        for file in files['Contents']:            
             if file['Key'].endswith('json'):
                 file_name = file['Key']
                 with open(file_name,'wb') as f:
-                    s3.download_fileobj('download-testing-10202020',file_name,f)
+                    s3.download_fileobj(WORKING_BUCKET,file_name,f)
 
                 with open(file_name, 'r') as f:
                     json_data = json.load(f)     
 
                 for order in json_data['orders']:
-                    data = order['order']['priceInfo']
-                    priceInfo_insert(data)
+                    price_data = order['order']['priceInfo']
+                    priceInfo_insert(price_data)
+
+                    taxprice_data = order['order']['taxPriceInfo']
+                    taxPriceInfo_insert(taxprice_data)
+
+                conn.commit()
+                conn.close()
 
                 copy_source = {
-                        'Bucket': 'download-testing-10202020',
+                        'Bucket': WORKING_BUCKET,
                         'Key': file_name
                     }
 
                 s3.copy(CopySource=copy_source,Bucket='archive-orders',Key=file_name)
-
-                s3.delete_object(Bucket='download-testing-10202020',Key='sample_order.json')
-
-        conn.commit()
-        conn.close()
-
+                # s3.delete_object(Bucket=WORKING_BUCKET,Key=file_name)
+            else:
+                pass
+    
     except (Exception, ps.DatabaseError) as error:
         print(error)
     finally:
@@ -77,7 +78,17 @@ def priceInfo_insert(data):
         print(error)
 
 
-
+def taxPriceInfo_insert(data):
+    try:
+        crsr = conn.cursor()
+        crsr.execute("""INSERT INTO taxpriceInfo(amount,currencycode,countytax,amountisfinal
+                        ,countrytax,discounted,statetax,citytax,districttax) 
+                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                     (data['amount'], data['currencyCode'], data['countyTax']
+                     , data['amountIsFinal'], data['countryTax'], data['discounted']
+                     , data['stateTax'], data['cityTax'], data['districtTax']))
+    except(Exception, ps.DatabaseError) as error:
+        print(error)
 
 
 if __name__ == "__main__":
